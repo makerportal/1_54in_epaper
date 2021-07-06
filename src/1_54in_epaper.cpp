af36150bbcfe6a984cce9961900991a9c772a402
Joshua Hrisko <49917891+josh-hrisko@users.noreply.github.com>
2021-07-05 17:30:14 -0700

    Add files via upload

---
+++ src/1_54in_epaper.cpp
@@ -0,0 +1,4 @@
+#include "Arduino.h"
+#include "1_54in_epaper.h"
+//#include "epd1in54_V2.h"
+//#include "epdpaint.h"
---
+++ src/1_54in_epaper.h
@@ -0,0 +1,7 @@
+#ifndef _1_54IN_EPAPER_H
+#define _1_54IN_EPAPER_H
+#include "epd1in54_V2.h"
+#include "epdpaint.h"
+#include <stdlib.h>
+
+#endif /* 1_54IN_EPAPER_H */
