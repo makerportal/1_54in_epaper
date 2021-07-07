1d10968db3003856896a632557ef20034d6b00dc
Joshua Hrisko <49917891+josh-hrisko@users.noreply.github.com>
2021-07-07 13:36:47 -0700

    Update BLE_text_display.ino

--- examples/BLE_text_display/BLE_text_display.ino
+++ examples/BLE_text_display/BLE_text_display.ino
@@ -21,10 +21,10 @@ Paint paint(image, 0, 0); // setup for text display
 
 String tot_val = ""; // storage variable for serial data
 
-String header_txt[4] = {"1.54in e-Paper","Simple Demo","Maker Portal"}; // header text
+String header_txt[3] = {"1.54in e-Paper","Simple Demo","Maker Portal"}; // header text
 
 int initial_space = 10; // initial white/dark space at the top of the display
-const int init_row_lines = 3; // counting rows to avoid header overlap
+const int init_row_lines = sizeof(header_txt)/sizeof(String); // counting rows to avoid header overlap
 int row_line = 0; // for iterating
 int row_height = 24; // row height (based on text size)
 
@@ -33,7 +33,7 @@ String line_array[max_lines-init_row_lines]; // More lines than this and the dis
 
 void setup() {
   Serial.begin(9600); // start serial handling for Bluetooth handling 
-  
+
   clear_func(); // clear display and set header
 }
 
@@ -44,24 +44,23 @@ void loop() {
     if (val=='\n'){ // wait for newline
       int str_len = strlen(tot_val.c_str()); // get length of string
       tot_val[str_len-1] = '\0'; // blank out the newline character from BLExAR
-      line_array[row_line] = tot_val; // append to string array
 
       if (row_line<(max_lines-init_row_lines)){
-        header_print(); // print header text
-        update_txt();
-        row_line+=1; // iterate to ensure new text prints to new line
-      } else {
-        row_line = 0; // reset if going beyond display
-        for (int qq=0;qq<(max_lines-1-init_row_lines);qq++){
-          line_array[qq] = ""; // reset array too
-        }
-        clear_func(); // clear entire e-paper and add header
-        delay(2000);
-        header_print(); // reintroduce header after clear
         line_array[row_line] = tot_val; // append to string array
-        update_txt(); // update with text
+        header_print(); // print header text
+        update_txt(row_line);
+      } else {
+        for (int qq=0;qq<(max_lines-init_row_lines);qq++){
+          line_array[qq] = String(" "); // reset array too
+        }
+        clear_func(); // clear display upon overload
+        row_line = 0; // reset iterator
+        line_array[row_line] = tot_val; // append to string array
+        header_print(); // print header
+        update_txt(row_line); // update with last received text
       }
       tot_val = ""; // clear serial input variable
+      row_line+=1; // iterate to ensure new text prints to new line
     }
   }
 }
@@ -69,7 +68,6 @@ void loop() {
 void header_print(){
   paint.SetWidth(200); // set display width
   paint.SetHeight(24); // set initial vertical space
-
   // loop through header text and print values
   for (int jj=0;jj<init_row_lines;jj++){
     paint.Clear(jj%2); // background
@@ -81,14 +79,15 @@ void header_print(){
 void clear_func(){
   epd.LDirInit(); // initialize epaper
   epd.Clear(); // clear old text/imagery
+  
   epd.DisplayPartBaseWhiteImage(); // lay a base white layer down first
-
+  
   header_print(); // print header text
   epd.DisplayPartFrame(); // display header text
 }
 
-void update_txt(){
-  for (int ii=0;ii<=row_line;ii++){
+void update_txt(int row_ii){
+  for (int ii=0;ii<=row_ii;ii++){
     paint.Clear(int(!bool(ii%2))); // clear background
     // below: draw at opposite to background (light vs dark) 
     paint.DrawStringAt(0, 4, line_array[ii].c_str(), &Font20, ii%2);
